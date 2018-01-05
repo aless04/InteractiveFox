df3d1764acd3af146c0535e38bc320949de434ed
aless04 <35148964+aless04@users.noreply.github.com>
2018-01-05 21:09:40 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,53 @@
+const Discord = require ("discord.js")
+var bot = new Discord.Client()
+var prefixbot = ("!")
+var GAME = 0
+var hasard = 0
+var prefix = ("/")
+
+bot.on('ready', () => {
+    console.log("Bot Ready !")
+})
+
+bot.login('Mzk4NDYyMTQ2ODg0NjY1MzU1.DS_BwQ.z6XSjBZSIJf0SkRE8Qbt5FZn-j8')
+
+bot.on('message', message => {
+    if(message.content == "Ping"){
+        message.reply("Pong")
+        console.log('ping pong')
+    }
+
+    if(message.content == prefix + "help"){
+        message.channel.sendMessage("__**Voici les commandes du bot:**__\n__/help__ : afficher les *commandes du bot*.\n__!roulette__ : jouer à la *roulette* (les règles sont éxpliquées une fois la commande efféctuée).")
+        console.log("Help demandé")
+    }
+
+    
+    if(message.content == prefixbot + "roulette" & message.channel.name == "general"){
+        message.reply("Tu ne peut pas utiliser cette commande ici,\nessaye le salon espace-bot :wink: !")
+    }
+    if((message.content == prefixbot + "roulette" & message.channel.name == "espace-bot") & (GAME == 1)){
+        message.reply("Tu ne peut pas effecteur cette commande tant qu'une autre partie de *roulette* est en cours !")
+    }
+    if((message.content == prefixbot + "roulette" & message.channel.name == "espace-bot") & (GAME == 0)){
+        message.channel.sendMessage("C'est partit pour la roulette :raised_hands: !")
+        message.channel.sendMessage("Tu dois essayer de trouver un nombre compris entre 0 et 1000 :neutral_face: \nLe premier à trouver le nombre gagne la partie :upside_down: !\nPour gagner il te suffit de dire un nombre et je te dirai si c'est + ou - :wink: ")
+        GAME = 1
+        hasard = Math.floor ( Math.random() * 1001 )
+
+    }
+    if(GAME === 1 & message.channel.name == "espace-bot"){
+        if(message.content == hasard){
+            message.reply("Bravo ! Tu remportes la partie !")
+            GAME = 0
+        }
+        if(message.content < hasard){
+            message.reply("c'est +")
+        }
+        if(message.content > hasard){
+            message.reply("c'est -")
+        }
+    }
+    
+})
+

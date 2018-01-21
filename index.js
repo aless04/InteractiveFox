b6cdc34bd5aa3af0ec61df6d56e40e34d9b15b6f
aless04 <35148964+aless04@users.noreply.github.com>
2018-01-21 15:51:26 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,4 @@
 const Discord = require ("discord.js")
-var embedroulette = {
- "embed": {
-    "color": 4886754,
-    "description": "**C'est partit pour la roulette ! :raised_hands:**\n\nTu dois essayer de trouver un nombre compris entre 0 et 1000 . :neutral_face:\n\nLe premier à trouver le nombre gagne la partie ! :upside_down:\n\nPour gagner il te suffit de dire un nombre et je te dirai si c'est + ou - ! :wink:",
-    "footer": {
-      "icon_url": "https://cdn.discordapp.com/embed/avatars/4.png",
-      "text": "Commande (!roulette)"
-    },
-    "author": {
-      "name": "Menu !roulette",
-      "url": "https://discordapp.com",
-      "icon_url": "https://cdn.discordapp.com/embed/avatars/2.png"
-    }
-  }
-}
 const embedhelp = {
     "embed": {
       "description": "\n**Voici toutes les commandes disponibles sur ce serveur Discord :**\n\n|``!help`` | Affiche le __Menu !help__ et toutes les autres commandes disponibles.|",
@@ -40,10 +25,7 @@ const embedhelp = {
   }
 var bot = new Discord.Client()
 var prefixbot = ("!")
-var GAME = 0
-var hasard = 0
-var prefix = ("/")
-var reacte = 0
+var reactemojisrandom = 0
 
 bot.on("ready", () => {
     console.log("Bot prêt prêt à être utilisé !")
@@ -93,38 +75,6 @@ bot.on('message', message => {
         message.react("☑")
        }
     }
-    if(message.content == prefixbot + "roulette" & message.channel.name == "general"){
-        message.reply("Tu ne peut pas utiliser cette commande ici,\nessaye le salon espace-bot :wink: !")
-    }
-    if((message.content == prefixbot + "roulette" & message.channel.name == "espace-bot") & (GAME == 1)){
-        message.reply("Tu ne peut pas effectuer cette commande tant qu'une autre partie de roulette est en cours !")
-    }
-
-    if((message.content == prefixbot + "roulette" & message.channel.name == "espace-bot") & (GAME == 0)){
-        message.channel.sendMessage(embedroulette)
-        GAME = 1
-        hasard = Math.floor ( Math.random() * 1001 )
-    }
-      if(GAME == 1  & message.channel.name == "espace-bot"){
-        if(message.author.bot){
-          return
-        }
-        else{
-
-        if(message.content == hasard){
-            message.react("✔")
-            message.reply("remporte la partie ! :raised_hands:")
-            GAME = 0
-        }
-        if(message.content < hasard){
-            message.react("➕")
-        }
-        if(message.content > hasard){
-            message.react("➖")
-        }
-      }
-    
-      }
  
 })
 bot.on("message", message => {

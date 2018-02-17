3fe110cdb7b2b2ad6b656ad513a2a3ed8e76af8e
aless04 <35148964+aless04@users.noreply.github.com>
2018-02-17 21:56:52 +0100

    Update index.js

--- index.js
+++ index.js
@@ -181,7 +181,7 @@ bot.on('message', message => {
     if(message.content.startsWith(prefixbot8ball) & message.channel.name == "general"){
         message.reply("Tu ne peut pas utiliser cette commande ici,\nessaye le salon espace-bot :wink: !")
     }
-    if(message.startsWith(prefixbot8ball) & message.channel.name == "espace-bot"){
+    if(message.content.startsWith(prefixbot8ball) & message.channel.name == "espace-bot"){
         hasard8ball = Math.floor ( Math.random() * 21 )
         if (hasard8ball == 1) {
             message.reply("Essaye plus tard")

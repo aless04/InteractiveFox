5297f12a1c4dccf4d6849d2e99b5959fa21c25ac
aless04 <35148964+aless04@users.noreply.github.com>
2018-02-18 12:59:17 +0100

    Update index.js

--- index.js
+++ index.js
@@ -362,3 +362,11 @@ bot.on('message', message => {
         }
     }
 })
+
+//logs//
+
+bot.on('message', message => {
+    if(message.content == test){
+        message.channel.id("411229117078568960").sendMessage("test")
+    }
+})
